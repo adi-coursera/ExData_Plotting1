6670e0d8a18a8fc1439d305a43e001ecf17bc23e
Adithya <adithya.gng@icloud.com>
2018-07-09 16:34:41 +0530

    Adding plot 2, plot2.R

--- plot2.R
+++ plot2.R
@@ -5,9 +5,6 @@ data$Global_active_power <- data[,as.numeric(Global_active_power)]
 data$Date <- data[,as.Date(Date,format="%d/%m/%Y")]
 data <- data[,datetime:=as.POSIXct(strptime(paste(Date,Time),format="%Y-%m-%d %H:%M:%S"))]
 png()
-plot(data$datetime,data$Global_active_power,type="n",xlab="",ylab="Energy sub metering")
-lines(data$datetime,data$Sub_metering_1,col="black")
-lines(data$datetime,data$Sub_metering_2,col="red")
-lines(data$datetime,data$Sub_metering_3,col="blue")
-legend("topright",legend=c("Submetering 1","Submetering 2","Submetering 3"),col=c("black","red","blue"),lty=1)
+with(data, plot(datetime,Global_active_power,type = "n",xlab = "",ylab = "Global Active Power(kilowatts)"))
+with(data, lines(datetime,Global_active_power))
 dev.off()
